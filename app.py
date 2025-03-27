58dfb53e4789db40a647b4f2965f5f4cd9385d87
AndyC <andyc2513@gmail.com>
2025-03-27 15:31:46 -0700

    added login

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import torch
+from huggingface_hub import login
 from collections.abc import Iterator
 from transformers import Gemma3ForCausalLM, AutoTokenizer, TextIteratorStreamer
 import time
 import spaces
 from threading import Thread
 import gradio as gr
+import os
 
+TOKEN = os.getenv("TOKEN")
+login(token=TOKEN)
 MAX_MAX_NEW_TOKENS = 2048
 DEFAULT_MAX_NEW_TOKENS = 1024
 MAX_INPUT_TOKEN_LENGTH = 4096
